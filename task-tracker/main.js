5239683e8b0ef3d4a547e84701102a22fe6f8cc4
Disney <disneyconstant996@gmail.com>
2025-01-24 19:01:27 +0300

    Refactor task input handling and enhance task operations with event delegation

--- task-tracker/main.js
+++ task-tracker/main.js
@@ -1,11 +1,47 @@
 const formElement = document.querySelector('form');
 const contentWrapper = document.querySelector('.content-wrapper');
 
-formElement.addEventListener('submit', (event) => {
 
+const taskArray = [];
+
+formElement.addEventListener('submit', (event) => {
+    const userSetTask = event.target.elements.task.value;
+    contentWrapper.appendChild(taskComponent(userSetTask))
+    taskArray.push(createObject(userSetTask, false));
+    event.preventDefault();
+    event.target.reset();
 });
 
 
+// Create a event delegation on the contentWrapper container for each taskComponent..
+contentWrapper.addEventListener('click', (event) => {
+    // console.log(event.target.parentNode)
+    if (event.target.classList.contains('task-status')) {
+        handleTaskStatus(event.target);
+    } 
+    if (event.target.closest('.task-delete')) {
+        const taskElement = event.target.closest('.task-item');
+        handleTaskDeletion(taskElement);
+    }
+})
+
+
+
+// Fuction for creating an object for every user input...
+function createObject(value, marked) {
+    const taskOjbect = {
+        description: value,
+        status: marked
+    }
+
+    return taskOjbect;
+}
+
+
+
+
+
+// Function component for creating a task component template..
 function taskComponent(desc) {
     const listItem = document.createElement('li');
     listItem.classList.add('task-item');
@@ -18,15 +54,6 @@ function taskComponent(desc) {
     deleteController.classList.add('task-delete');
     deleteController.innerHTML = `<ion-icon name="trash"></ion-icon>`;
 
-    statusController.addEventListener('click', (event) => {
-        statusController.classList.toggle('marked');
-        para.classList.toggle('strikethrough');
-    })
-
-    deleteController.addEventListener('click', (event) => {
-        listItem.remove();
-    })
-
     listItem.appendChild(statusController);
     listItem.appendChild(para);
     listItem.appendChild(deleteController);
@@ -35,4 +62,18 @@ function taskComponent(desc) {
 }
 
 
-// contentWrapper.appendChild(taskComponent('Marking in complete will put it back in pending list'));
+
+// Functions for task operations
+function handleTaskStatus(statusElement) {
+    const paraElement = statusElement.nextElementSibling;
+    statusElement.classList.toggle('marked');
+    paraElement.classList.toggle('strikethrough');
+}
+
+function handleTaskDeletion(taskElement) {
+    taskElement.remove();
+}
+
+
+
+// Trying some tes
